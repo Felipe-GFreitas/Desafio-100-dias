bda0d781d0be54b109e7b6ce1bdad8e5ba1dd75b
Felipe <flp266.fg@gmail.com>
2024-05-04 18:17:51 -0300

    Dia 07, 1 semana :D

---
+++ 007.py
@@ -0,0 +1,7 @@
+'''
+Basicamente uma soma, e assim finalizamos a primeira semana
+https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+'''
+
+def aVeryBigSum(ar):
+    return sum(map(int, ar))
