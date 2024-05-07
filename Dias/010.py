c6ab18e1ac4926f9896659bbf2a90afbc57a83e5
Felipe <flp266.fg@gmail.com>
2024-05-07 20:00:10 -0300

    Dia 010

---
+++ Dias/010.py
@@ -0,0 +1,24 @@
+import math
+import os
+'''
+Exercicio tirado do HackerRank
+https://www.hackerrank.com/challenges/encryption/problem?isFullScreen=true
+'''
+def encryption(s):
+    L = len(s)
+    rows = int(math.floor(L**(0.5)))
+    columns = int(math.ceil(L**(0.5)))
+    output = ""
+    for i in range(columns):
+        k = i
+        for j in range(k,L,columns):
+            output+=s[j]
+        output+=" "
+    return output
+
+
+
+# Exemplo de uso:
+s = "ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots"
+result = encryption(s)
+print(result)
